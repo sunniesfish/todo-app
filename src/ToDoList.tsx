46cb3fe1931fb253666bd3e89ff8f7e4f32df957
sunniesfish <hhch5611.jh@gmail.com>
2024-05-09 18:39:35 +0900

    #6.7 Form Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -31,14 +31,44 @@ import { useForm } from "react-hook-form";
 // }
 
 function ToDoList() {
-    const { register, watch } = useForm();
+    const { register, watch, handleSubmit, formState } = useForm();
     
-
-    console.log(watch())
+    const onValid = (data:any) => {
+    }
+    console.log(formState.errors);
     return (
         <div>
-            <form >
-                <input {...register("todo")} placeholder="Write a to do"/>
+             <form
+                style={{ display: "flex", flexDirection: "column" }}
+                onSubmit={handleSubmit(onValid)}
+                >
+                <input {...register("email", { required: true })} placeholder="Email" />
+                <input
+                    {...register("firstName", { required: true })}
+                    placeholder="First Name"
+                />
+                <input
+                    {...register("lastName", { required: true })}
+                    placeholder="Last Name"
+                />
+                <input
+                    {...register("username", { required: true, minLength: 10 })}
+                    placeholder="Username"
+                />
+                <input
+                    {...register("password", { required: true, minLength: 5 })}
+                    placeholder="Password"
+                />
+                <input
+                    {...register("password1", {
+                    required: "Password is required",
+                    minLength: {
+                        value: 5,
+                        message: "Your password is too short.",
+                    },
+                    })}
+                    placeholder="Password1"
+                />
                 <button>Add</button>
                 {}
             </form>
