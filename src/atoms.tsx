6c3fb672361029872aff4b30758fa52012bf3b42
sunniesfish <hhch5611.jh@gmail.com>
2024-05-13 15:50:08 +0900

    #6.17 Selectors part Two

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,4 +1,4 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export interface IToDo {
     text:string;
@@ -10,3 +10,17 @@ export const toDoState = atom<IToDo[]>({
     key:"toDo",
     default:[],
 });
+
+export const categoryState =atom<"DONE"|"DOING"|"TO_DO">({
+    key:"toDo",
+    default:"TO_DO"
+})
+
+
+export const toDoSelector = selector({
+    key:"toDoSelector",
+    get: ({get}) => {
+        const toDos = get(toDoState);
+        const category = get(categoryState);
+        return toDos.filter((todo) => todo.category === category)
+})
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,6 +1,6 @@
-import {  useRecoilValue } from "recoil";
+import {  useRecoilState, useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoState } from "../atoms";
+import { IToDo, categoryState, toDoSelector, toDoState } from "../atoms";
 import ToDo from "./ToDo";
 
 
@@ -11,16 +11,27 @@ import ToDo from "./ToDo";
 function ToDoList(){
     // const [value, modFn] = useRecoilState(toDoState); // [value, modFn]을 받음. useState과 유사하다. 이렇게 한번에 둘다 받거나 아래처럼 따로 받거나
     const toDos = useRecoilValue(toDoState); //빈 배열 받기
-
+    const [toDo, doings, dones] = useRecoilValue(toDoSelector);
+    const [category, setCategory] = useRecoilState(categoryState)
+    const onInput = (event:React.FormEvent<HTMLSelectElement>) => {
+        setCategory(event.currentTarget.value);
+    }
     return (
-        <div>
-            <h1>To Dos</h1>
-            <hr />
-                <CreateToDo/>
-            <ul>
-                {toDos.map(todo => <ToDo key={todo.id} {...todo}/>)}
-            </ul>
-        </div>
+        <>
+            <div>
+                <h1>To Dos</h1>
+                <hr />
+                    <select onInput={onInput}>
+                        <option value="TO_DO">To Do</option>
+                        <option value="DOING">Doing</option>
+                        <option value="DONE">Done</option>
+                    </select>
+                    <CreateToDo/>
+                    {toDos.map( todo => (
+                        <ToDo key={todo.id} {...todo}/>
+                    ))}
+            </div>
+        </>
     );
 }
 
